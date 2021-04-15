e5fc51a21bc7c7f40f5f7b414ad3c1d5375fc6ae
Garin Mccullick <glmccullick@gmail.com>
2021-04-15 17:06:08 -0500

    I did it from scratch!

--- src/App.js
+++ src/App.js
@@ -1,15 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import "./App.css";
+import "./index.js";
+import styled from 'styled-components'
 
-function App() {
+export default function App() {
+  const [pic,setPic] = useState([])
+
+  useEffect(()=>{
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=aMD4uPFAWDu9f0NbamzRt5xW4f8uQ3jLmtFFzEv4')
+    .then(res => {
+      setPic(res.data)
+      console.log(res.data);
+    })
+    .catch(err =>{
+      console.log(err);
+    })
+  }, []);
+
+  const StyledDiv = styled.div`
+  background-image:url(${pic.hdurl});
+  height:100%;
+  background-repeat:no-repeat;
+  background-size:cover;
+  background-position:center;
+  `
+  
   return (
-    <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
-    </div>
+    <body>
+      <div className="App-header">
+        <nav>
+          {pic.explanation}
+        </nav>
+      </div>
+      <StyledDiv>
+        <div></div>
+      </StyledDiv>
+      </body>
   );
 }
 
-export default App;
+ 
